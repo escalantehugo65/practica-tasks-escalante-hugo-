60f51005407ec47b49157ca9111ce9ffee7a0c13
escalantehugo65 <palofito40@gmail.com>
2026-08-28 12:40:55 -0300

    Feat: Se ha añadido la eliminación lógica y matchedData

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 import { UserModel as User } from "../models/user.model.js";
+import{matchedData} from 'express-validator'
 
 export const getUsers = async (req, res) => {
   try {
@@ -42,38 +43,36 @@ export const createUser = async (req, res) => {
     } catch (error) {
         console.log("Error capturado:", error.message);
         return res.status(500).json({
-            message: "Error al crear el usuario",
+            message: ("Error al crear el usuario"),
             error: error.message
         });
     }
 };
 
 export const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, email, password } = req.body;
+try {
+  const { id } = req.params;
 
-    const user = await User.findByPk(id);
+  const datosLimpios = matchedData(req);
 
-    const existingUser = await User.findOne({ where: { email } });
-
-
-    await user.update({
-      name: name.trim(),
-      email: email.trim(),
-      password,
-    });
-
-    return res.status(200).json({
-      message: "Usuario actualizado correctamente",
-      user,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error al actualizar el usuario",
-      error: error.message,
-    });
+  if(Object.keys(datosLimpios).length===0){
+    return res.status(400).json({
+      message:("No se enviaron datos validos, intente de nuevo ingresando los datos de manera correcta")
+  })
   }
+
+  await User.update(datosLimpios,{where:{id}});
+
+  return res.status(200).json({
+    message:"Usuario actualizado de manera correcta",
+    datos_actualizados: datosLimpios
+});
+} catch(error){
+  return res.status(500).json({
+    message:"Error interno al realizar la actualización del usuario, intente mas tarde",
+    error: error.message
+})
+}
 };
 
 export const deleteUser = async (req, res) => {
@@ -85,7 +84,7 @@ export const deleteUser = async (req, res) => {
     await user.destroy();
 
     return res.status(200).json({
-      message: "Usuario eliminado correctamente",
+      message: "Usuario eliminado logicamente de manera correcta",
     });
   } catch (error) {
     return res.status(500).json({
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -15,6 +15,9 @@ const UserModel = baseDatos.define("User", {
     type: DataTypes.STRING(100),
     allowNull: false,
   },
+},{
+  timestamps: true,
+  paranoid: true
 });
 
 export { UserModel };
